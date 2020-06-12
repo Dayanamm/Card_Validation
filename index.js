383172ecefc72307328dc1216157d8318aa065a1
Dayana M <63815530+Dayanamm@users.noreply.github.com>
2020-06-12 18:00:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-Implementar política de datos.
+//Implementar política de datos.
 alert("Acepto la política para el Tratamiento de Protección de Datos Personales de acuerdo con la Ley Estatutaria 1581 de 2012.");
 
 //Implementamos preventDefault para evitar que recargue la página y no lea nuestra función. 
