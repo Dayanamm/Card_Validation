9d7e37a9b199af402dfbb589a6c414676cc46e7e
Dayana M <63815530+Dayanamm@users.noreply.github.com>
2020-06-12 17:59:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-//Implementar política de datos.
-//alert("Acepto la política para el Tratamiento de Protección de Datos Personales de acuerdo con la Ley Estatutaria 1581 de 2012.");
+Implementar política de datos.
+alert("Acepto la política para el Tratamiento de Protección de Datos Personales de acuerdo con la Ley Estatutaria 1581 de 2012.");
 
 //Implementamos preventDefault para evitar que recargue la página y no lea nuestra función. 
 addEventListener("submit", function(event){
 event.preventDefault()
 });
-
-/*//Apagar el preventDefault.
-function apagar(){
-document.getElementById("id").removeEventListener("onclick", isValidCard);
-}*/
 
 //Declaramos función para traer el dato.
 function mostrar(){
